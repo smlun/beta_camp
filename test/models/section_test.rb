3ef4d09024f4098daf4b01a3d63536289c550b9c
Ming Lun <smlun@msn.com>
2017-03-06 15:38:49 +0800

    Test models association

--- test/models/section_test.rb
+++ test/models/section_test.rb
@@ -5,9 +5,17 @@ class SectionTest < ActiveSupport::TestCase
   #   assert true
   # end
 
+  def setup
+    @section = sections(:valid)
+  end
+
   test 'section should have valid name' do
     section = Section.new(name: 'name')
     assert section.valid?
+  end
+
+  test '#posts' do
+    assert_equal 2, @section.posts.size
   end
 
 end
