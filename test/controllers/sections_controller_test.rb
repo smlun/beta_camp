83bed4ee719e36d230256420440cef74bf78a29a
Ming Lun <smlun@msn.com>
2017-03-06 16:11:17 +0800

    Test sections controller

--- test/controllers/sections_controller_test.rb
+++ test/controllers/sections_controller_test.rb
@@ -4,4 +4,16 @@ class SectionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should get index' do
+    get sections_url
+    assert_response :success
+  end
+
+  test 'should create section' do
+    assert_difference('Section.count') do
+      post sections_url, params: { section: { name: 'Rails is awesome' } }
+    end
+    assert_redirected_to section_path(Section.last)
+  end
 end
