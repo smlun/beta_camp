b5555d2472c3c714704efd97be21db5bba5af7d5
Ming Lun <smlun@msn.com>
2017-03-06 16:11:44 +0800

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sections do
+    resources :posts
+  end
+  get 'sections/index'
+  root 'sections#index'
 end
