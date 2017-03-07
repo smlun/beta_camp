777782f6f3a7c26f1ea9ac829331174ad37a8195
Ming Lun <smlun@msn.com>
2017-03-07 13:35:01 +0800

    Implement CRUD methods for Section controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,35 @@
 class SectionsController < ApplicationController
+  def index
+    @sections = Section.all
+  end
+
+  def new
+    @section = Section.new()
+  end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def create
+    @section = Section.new(section_params)
+    @section.save
+    redirect_to @section
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+    @section.save
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  private
+    def section_params
+      params.require(:section).permit(:name)
+    end
 end
