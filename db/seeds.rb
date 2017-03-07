a5f04ebd701776168dac38dd6266949565507347
Ming Lun <smlun@msn.com>
2017-03-07 13:43:03 +0800

    Update seed data with references

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,34 @@ sections = Section.create([{ name: 'Ruby Basics' },
                            { name: 'CSS'}
                           ])
 
-posts = Post.create([{ link: "https://launchschool.com/books/ruby/read/intro_exercises" , description: "Start on this to understand basic ruby concepts"},
-                     { link: "http://guides.rubyonrails.org/getting_started.html" , description: "Start on this to familiarize yourself with rails"},
-                     { link: "https://www.w3schools.com/html/", description: "Read this to understand basic HTML"},
-                     { link: "https://www.w3schools.com/css/default.asp", description: "Basic CSS. Read all."},
-                     { link: "http://getbootstrap.com/", description: "Test with Bootstrap. Read the getting started link."},
-                     { link: "hhttps://www.w3schools.com/bootstrap/default.asp", description: "Read bootstrap CSS for clarity"}
+posts = Post.create([{
+                       link: "https://launchschool.com/books/ruby/read/intro_exercises",
+                       description: "Start on this to understand basic ruby concepts",
+                       section_id: 1
+                     },
+                     {
+                       link: "http://guides.rubyonrails.org/getting_started.html",
+                       description: "Start on this to familiarize yourself with rails",
+                       section_id: 1
+                     },
+                     {
+                       link: "https://www.w3schools.com/html/",
+                       description: "Read this to understand basic HTML",
+                       section_id: 2
+                     },
+                     {
+                       link: "https://www.w3schools.com/css/default.asp",
+                       description: "Basic CSS. Read all.",
+                       section_id: 3
+                     },
+                     {
+                       link: "http://getbootstrap.com/",
+                       description: "Test with Bootstrap. Read the getting started link.",
+                       section_id: 3
+                     },
+                     {
+                       link: "hhttps://www.w3schools.com/bootstrap/default.asp",
+                       description: "Read bootstrap CSS for clarity",
+                       section_id: 3
+                     }
                     ])
