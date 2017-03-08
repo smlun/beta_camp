09a09c03ac277583244575c1d1c91c9e33c7b6dd
Ming Lun <smlun@msn.com>
2017-03-08 17:00:32 +0800

    Implement Rabl gem

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::PostsController < ApplicationController
   def index
     @posts = Post.all
-    respond_to do |format|
-      format.json { render :json => @posts.to_json, :status => :ok }
-    end
   end
 
   def create
---
+++ app/views/api/v1/posts/index.rabl
@@ -0,0 +1,4 @@
+collection @posts
+attributes :links, :description, :post_image
+# node(:hello) {|a| "hello"}
+# child(:user)
